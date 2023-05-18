11a1641b768de0a1b4b6fa6c5ffaa8e1143ca009
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-05-18 18:54:34 -0400

    troubleshooting routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,17 +2,29 @@ const router = require("express").Router();
 const {User, Talk, Question} = require("../models");
 const withAuth = require("../utils/auth");
 
-
-
-router.get("/login", (req, res) => {
-    
-    if (req.session.logged_in){
-        res.redirect("/");
-        return;
-    }
-
-    res.render("login");
-    
+router.get("/", async (req, res) => {
+    const talkData = await Talk.findAll( {
+        attributes: ["talk_id", "name", "description", "date", "user_id"],
+        include: [{model: Question, attributes: ["question_id", "question", "created", "user_id", "talk_id"],
+                    include: {model: User, attributes: ["name"]},}]
+    }).catch((error) => {res.status(500).json(error)});
+    const talk = talkData.map((talk) => talk.get({ plain: true }));
+    console.log(talk)
+    res.render('homepage', {
+        talk,
+        logged_in: req.session.logged_in,
+    })
 });
+
+// router.get("/login", (req, res) => {
+    
+//     if (req.session.logged_in){
+//         res.redirect("/");
+//         return;
+//     }
+
+//     res.render("login");
+    
+// });
 
 module.exports = router;
