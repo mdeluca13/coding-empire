32d8e54fa677a45fc89fdf114cb9a543fdaa79f0
Davis-Cedrick Baseka <base0005@algonquinlive.com>
2023-05-25 19:00:29 -0400

    talkPage style Done

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,25 +1,23 @@
 module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
-    format_date: (date) => {
-      const format = require('date-fns/format')
-      if (date == null){
-        return "TBD";
-      }
-      else {
-        return format(new Date(date), "MMMM do', ' yyyy",);
-      }
-      
-    },
-    distance: (date) => {
-      const formatDistanceStrict = require('date-fns/formatDistanceStrict');
-      if (date == null){
-        return "TBD";
-      }
-      else {
-        return formatDistanceStrict(new Date(), date, {unit: 'day'});
-      }
-      
+  format_time: (date) => {
+    return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    const format = require('date-fns/format')
+    if (date == null){
+      return 'TBD';
     }
+    else {
+      return format(new Date(date), 'MMMM do', ' yyyy',);
+    }
+  },
+  distance: (date) => {
+    const formatDistanceStrict = require('date-fns/formatDistanceStrict');
+    if (date == null){
+      return 'TBD';
+    }
+    else {
+      return formatDistanceStrict(new Date(), date, {unit: 'day'});
+    }
+  }
 };
